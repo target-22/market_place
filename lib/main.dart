b0c9f99ad409a71922a66a6fc1f053ac53496b16
Dell <eheba0302@gmail.com>
2023-10-03 17:09:54 +0300

    add assets

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MarketPlace());
+}
+
+class MarketPlace extends StatelessWidget {
+ // const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
+
+
+
+
+
